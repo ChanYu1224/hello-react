91a1a80df92e9eb9d4a1e3e099dbcbf2abe8e3e5
Yuta Nishikawa <kinkitunekin@gmail.com>
2024-05-30 15:12:34 +0900

    [ADD] japanese aibilties

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { usePokemon } from "../hooks/usePokemon";
+import { usePokemon, usePokemonAbility } from "../hooks/usePokemon";
 
 
 export default function Pokemon() {
     const { id } = useParams();
-    const { data: pokemon, isLoading, error } = usePokemon(Number(id));
+    const { data: pokemon, isLoading: isPokemonLoading, error: pokemonError } = usePokemon(Number(id));
+    const { data: abilities, isLoading: isAbilityLoading, error: abilityError} = usePokemonAbility(pokemon?.abilityUrls);
 
-    if (isLoading) {
+    if (isPokemonLoading) {
         return <div>Loading Now...</div>;
     }
-    if (error) {
-        return <div>Error on Loading!</div>;
+    if (pokemonError) {
+        return <div>Error on Loading</div>;
+    }
+    
+    // pokemonのloadに成功したら以下を実行
+    let abilityElements;
+    if (isAbilityLoading) {
+        abilityElements = <div>Loading Now...</div>;
+    } else if (abilityError) {
+        abilityElements = <div>Error on Loading</div>
+    } else {
+        abilityElements = abilities.map((value, index) => {
+            return (
+                <li key={index}>{value}</li>
+            );
+        });
     }
 
     return (
@@ -20,6 +35,10 @@ export default function Pokemon() {
             <p>name: {pokemon?.name}</p>
             <p>weight: {pokemon?.weight}</p>
             <p>height: {pokemon?.height}</p>
+            <p>abilities: </p>
+            <ul>
+                {abilityElements}
+            </ul>
         </div>
     );
 }
--- src/hooks/usePokemon.tsx
+++ src/hooks/usePokemon.tsx
@@ -1,17 +1,41 @@
 import useSWR from "swr";
 
-type Result = {
+type PokemonResult = {
     name: string,
     weight: number,
     height: number,
+    abilityUrls: Array<string>,
 };
 
-const fetcher = (key: string) => fetch("https://pokeapi.co/api/v2/"+ key)
+const pokemonFetcher = (key: string) => fetch("https://pokeapi.co/api/v2/"+ key)
     .then((response) => (response.ok ? response.json() : Promise.reject(response)))
-    .then((response) => response as Result);
+    .then((response) => {
+        return {
+            name: response.name,
+            weight: response.weight,
+            height: response.height,
+            abilityUrls: response.abilities.map((value) => value.ability.url),
+        } as PokemonResult;
+    });
 
 
 export function usePokemon(id?: number) {
     const key = id ? `pokemon/${id}` : null;
-    return useSWR(key, fetcher);
+    return useSWR(key, pokemonFetcher);
 };
+
+// refer: https://countstheclouds.com/posts/nextjs-useswr-multiple-loading/
+function pokemonAbilityFetcher(urls: Array<string>) {
+    const fetcher = (url) => fetch(url).then((response) => (response.ok? response.json() : Promise.reject(response))
+        .then((response) => {
+            const effectNames: Array<any> = response.names;
+            const abilityName = effectNames.find((value) => value.language.name === "ja").name;
+            return abilityName;
+        }
+    ));
+    return Promise.all(urls.map(fetcher));
+}
+
+export function usePokemonAbility(abilityUrls: Array<string>) {
+    return useSWR(abilityUrls, pokemonAbilityFetcher);
+}
